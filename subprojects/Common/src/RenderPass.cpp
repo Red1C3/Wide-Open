aef099e8394a9dcd5a50971b6048008559962fcf
Mohammad <2000issawi@gmail.com>
2021-04-20 23:56:37 +0300

    Same as the last commit,just forgot to stage a file :/

--- subprojects/Common/src/RenderPass.cpp
+++ subprojects/Common/src/RenderPass.cpp
@@ -2,8 +2,8 @@
 using namespace Common;
 RenderPass::RenderPass(){}
 RenderPass::~RenderPass(){}
-void RenderPass::init(VkDevice* device){
-    this->device=device;
+void RenderPass::init(Renderer* renderer){
+    this->renderer=renderer;
     setupAttachments();
     setupSubPasses();
     if(!(attachments&&attachmentsRefs&&subPasses&&depenedncies)){
@@ -13,18 +13,32 @@ void RenderPass::init(VkDevice* device){
     createInfo.sType=VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
     createInfo.attachmentCount=attachmentsCount;
     createInfo.pAttachments=attachments;
-    createInfo.subpassCount=subpassesCount;
+    createInfo.subpassCount=subPassesCount;
     createInfo.pSubpasses=subPasses;
-    createInfo.dependencyCount=subpassesCount;
+    createInfo.dependencyCount=dependenciesCount;
     createInfo.pDependencies=depenedncies;
-    if(vkCreateRenderPass(*device,&createInfo,ALLOCATOR,&renderPass)!=VK_SUCCESS){
+    if(vkCreateRenderPass(renderer->getDevice(),&createInfo,ALLOCATOR,&renderPass)!=VK_SUCCESS){
         LOG.error("Failed to create renderpass");
     }
+    LOG.log("Created renderpass successfully");
     setupFramebuffers();
     if(!framebuffers){
         LOG.error("Framebuffers were not setup properly");
     }
 }
 void RenderPass::terminate(){
-    //TODO implement
+    vkDestroyRenderPass(renderer->getDevice(),renderPass,ALLOCATOR);
+    for(uint32_t i=0;i<framebuffersCount;++i){
+        vkDestroyFramebuffer(renderer->getDevice(),framebuffers[i].framebuffer,ALLOCATOR);
+        for(uint32_t j=0;j<framebuffers[i].imagesCount;++j){
+            vkDestroyImageView(renderer->getDevice(),framebuffers[i].imageViews[j],ALLOCATOR);
+            vkDestroyImage(renderer->getDevice(),framebuffers[i].images[j],ALLOCATOR);
+            vkFreeMemory(renderer->getDevice(),framebuffers[i].imagesMemory[j],ALLOCATOR);
+        }
+    }
+    delete[] framebuffers;
+    delete[] attachments;
+    delete[] attachmentsRefs;
+    delete[] subPasses;
+    delete[] depenedncies;
 }
